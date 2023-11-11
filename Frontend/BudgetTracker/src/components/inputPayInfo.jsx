56c8b66c13abc4c6fc9bdd7efaddc715bc39c7ce
Patel Divy <divypatel0403@gmail.com>
2023-11-11 10:59:05 +0530

    Created the backend and the frontend for the input fields

--- Frontend/BudgetTracker/src/components/inputPayInfo.jsx
+++ Frontend/BudgetTracker/src/components/inputPayInfo.jsx
@@ -1,63 +1,224 @@
-// import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import styles from "../assets/inputPayInfo.module.css";
-import "../App.css";
-export default function FormPropsTextFields() {
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import Autocomplete from "@mui/material/Autocomplete";
+import axios from 'axios';
+
+export default function PaymentForm() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [paymentMode, setPaymentMode] = useState("");
+  const [amount, setAmount] = useState("");
+  const [receiverEmail, setReceiverEmail] = useState("");
+  const [senderEmail, setSenderEmail] = useState("");
+
+  const handleModeChange = (event) => {
+    setPaymentMode(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      category: selectedCategory,
+      paymentMode: paymentMode,
+      amount: amount,
+      receiverEmail: receiverEmail,
+      senderEmail: senderEmail,
+    };
+
+    try {
+      await axios.post("YOUR_API_ENDPOINT", formData).then((response) => {
+        console.log(response.data);
+      });
+      setSelectedCategory("");
+      setPaymentMode("");
+      setAmount("");
+      setReceiverEmail("");
+      setSenderEmail("");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const Categories = [
+    "Food",
+    "Entertainment",
+    "Rentals",
+    "Clothes",
+    "Utilities",
+    "Transportation",
+    "Healthcare",
+    "Education",
+    "Travel",
+    "Groceries",
+    "Personal Care",
+    "Home Maintenance",
+    "Electronics",
+    "Gifts/Donations",
+    "Insurance",
+    "Dining Out",
+    "Hobbies",
+    "Subscriptions",
+    "Miscellaneous",
+  ];
+
   return (
     <Box
       component="form"
+      onSubmit={handleSubmit}
       sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
+        m: 1,
+        width: "40ch",
+        display: "flex",
+        flexDirection: "column",
       }}
       noValidate
       autoComplete="on"
     >
-      <div>
-        <h1 className="text-gray-200">Hello World</h1>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={Categories}
+        getOptionLabel={(option) => option}
+        onChange={(event, newValue) => {
+          setSelectedCategory(newValue);
+        }}
+        sx={{
+          cursor: "pointer",
+          fontWeight: 900,
+          m: 1,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "blue",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#00BFFF",
+            },
+          },
+          "& .MuiSelect-select": {
+            padding: "10px",
+            fontSize: "16px",
+            height: "50px", // Set the height of the select input
+            display: "flex", // Use flexbox to center the text vertically
+            alignItems: "center", // Center the text vertically
+          },
+        }}
+        renderInput={(params) => <TextField {...params} label="Category" />}
+      />
+      <TextField
+        required
+        id="paymentMode"
+        label="Payment Mode"
+        select
+        value={paymentMode}
+        onChange={handleModeChange}
+        sx={{
+          cursor: "pointer",
+          fontWeight: 900,
+          m: 1,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "blue",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#00BFFF",
+            },
+            "& .MuiSelect-select": {
+              fontSize: "16px",
 
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Category"
-          defaultValue="Category"
-          // className={styles.textField}
-          className='border-2'
-        />
-        <TextField required id="outlined" label="" defaultValue="Hello World" />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />{" "}
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />{" "}
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-      </div>
+              display: "flex", // Use flexbox to center the text vertically
+              alignItems: "center", // Center the text vertically
+            },
+          },
+        }}
+      >
+        <MenuItem value="cash">Cash</MenuItem>
+        <MenuItem value="online">Online</MenuItem>
+      </TextField>
+
+      <TextField
+        required
+        id="amount"
+        label="Amount"
+        defaultValue="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        sx={{
+          cursor: "pointer",
+          fontWeight: 900,
+          m: 1,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "blue",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#00BFFF",
+            },
+          },
+        }}
+      />
+      <TextField
+        required
+        id="receiver"
+        label="Receiver's Phone Number"
+        // defaultValue=""
+        value={receiverEmail}
+        onChange={(e) => setReceiverEmail(e.target.value)}
+        sx={{
+          cursor: "pointer",
+          fontWeight: 900,
+          m: 1,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "blue",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#00BFFF",
+            },
+          },
+        }}
+      />
+      <TextField
+        required
+        id="sender"
+        label="Sender's Phone Number"
+        // defaultValue=""
+        value={senderEmail}
+        onChange={(e) => setSenderEmail(e.target.value)}
+        sx={{
+          cursor: "pointer",
+          fontWeight: 900,
+          m: 1,
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "black",
+            },
+            "&:hover fieldset": {
+              borderColor: "blue",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#00BFFF",
+            },
+          },
+        }}
+      />
+
+      <Button variant="contained" type="submit">
+        Submit
+      </Button>
     </Box>
   );
 }
