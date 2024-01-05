9fc2de8a79392ee793bb2b532b83eb9331f57a0c
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2024-01-05 21:57:40 +0530

    SignUp Page

--- Frontend/BudgetTracker/src/components/Pages/signUp.jsx
+++ Frontend/BudgetTracker/src/components/Pages/signUp.jsx
@@ -134,21 +134,22 @@ export default function SignUp() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 py-12 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div className="flex flex-col items-start bg-no-repeat bg-cover bg-center
+		bg-[url('../public/images/loginimg.jpg')] justify-center min-h-screen w-full" >
+      <div className="max-w-md w-full space-y-8 mb-32 mx-56">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Sign Up
         </h2>
         <form className="mt-8 space-y-6">
           <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
+          <div className="">
+            <div className="">
               <input
                 id="email"
                 name="email"
                 type="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none my-4 shadow-sm rounded-md flex w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
               />
             </div>
@@ -158,7 +159,7 @@ export default function SignUp() {
                 name="password"
                 type="password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-nonemy-4 shadow-sm rounded-md flex w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
             </div>
