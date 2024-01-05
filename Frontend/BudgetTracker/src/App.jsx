1f8aa039a478a5396140f6aa2983f7e88d82f1be
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2024-01-05 10:51:09 +0530

    Conflict resolved

--- Frontend/BudgetTracker/src/App.jsx
+++ Frontend/BudgetTracker/src/App.jsx
@@ -7,12 +7,14 @@ import './App.css'
 import ModeTracker from './components/trackerBox';
 import IntputPayInfo from './components/inputPayInfo';
 import signUp  from './components/Pages/signUp';
-import Login from './components/Pages/login';
 function App() {
   return (
-    <>
-    <IntputPayInfo></IntputPayInfo>
-    {/* <ModeTracker></ModeTracker>      */}
+    <>  
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<signUp />}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
